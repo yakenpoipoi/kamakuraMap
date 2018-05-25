b17e900f38cf7304a42cfd3b87dc9753c3167cb1
Yoshiki <kishimoto0126@icloud.com>
2018-05-25 15:06:03 +0900

    アーカイブ

--- kamakuraMap/SecondViewController.swift
+++ kamakuraMap/SecondViewController.swift
@@ -32,6 +32,8 @@ class SecondViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
